3293698b5cdaaab8ad26a74e5d428ebc8604464c
binoll <onionlsfa@gmail.com>
2023-07-17 02:42:15 +0300

    feat: added animation background class

--- widgets/widgets.cpp
+++ widgets/widgets.cpp
@@ -1,1 +1,33 @@
 #include "widgets.hpp"
+
+Background::Background(const std::vector<std::string>& vector_paths,
+                       sf::Clock& clock,
+                       float switch_time) : clock(clock), switch_time(switch_time) {
+    sf::Texture texture;
+
+    for (const auto& vector_path : vector_paths) {
+        texture.loadFromFile(vector_path);
+        this->vector_textures.push_back(texture);
+    }
+}
+
+sf::Texture Background::update_texture() {
+    static short texture_number = 0;
+    sf::Texture texture = this->vector_textures[texture_number];
+
+    if (clock.getElapsedTime().asSeconds() >= switch_time) {
+        texture_number = (texture_number + 1) % this->vector_textures.size();
+        texture = this->vector_textures[texture_number];
+        clock.restart();
+    }
+
+    return texture;
+}
+
+void Background::draw(sf::RenderWindow& window) {
+    sf::Texture texture = this->update_texture();
+    sf::Sprite sprite(texture);
+
+    window.draw(sprite);
+}
+
